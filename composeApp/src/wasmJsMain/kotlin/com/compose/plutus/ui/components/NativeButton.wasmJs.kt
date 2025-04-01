841d83a284dbb8fcbc3e43cf888d3a9800b6774d
Angela <angelabosilkovska99@gmail.com>
2025-04-01 17:18:46 +0200

    new native button for web

--- composeApp/src/wasmJsMain/kotlin/com/compose/plutus/ui/components/NativeButton.wasmJs.kt
+++ composeApp/src/wasmJsMain/kotlin/com/compose/plutus/ui/components/NativeButton.wasmJs.kt
@@ -1,5 +1,8 @@
 package com.compose.plutus.ui.components
 
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -12,5 +15,14 @@ actual fun NativeButton(
     backgroundColor: Color,
     textColor: Color,
 ) {
-
+    Button(
+        onClick = onClick,
+        modifier = modifier,
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = backgroundColor,
+            contentColor = textColor
+        )
+    ) {
+        Text(label)
+    }
 }
