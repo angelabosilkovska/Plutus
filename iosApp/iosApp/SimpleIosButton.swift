31fab4e1ab56d8994091cd91cdc2d2259021c3ee
Angela <angelabosilkovska99@gmail.com>
2025-04-01 18:02:13 +0200

    updated the native ios button to have e better color

--- iosApp/iosApp/SimpleIosButton.swift
+++ iosApp/iosApp/SimpleIosButton.swift
@@ -26,6 +26,29 @@ struct SimpleIosButton: View {
         Button(action: action) {
             Text(label)
                 .font(.headline)
+                .foregroundColor(Color(hex: "#FFFFFF")) // Set text color
+                .padding() // Add some padding for better appearance
+                .background(Color(hex: "#4C662B")) // Set background color
+                .cornerRadius(8) // Optional: Add rounded corners
         }
+    }
+}
+
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        switch hex.count {
+        case 6: // RGB (no alpha)
+            (a, r, g, b) = (255, (int >> 16) & 0xFF, (int >> 8) & 0xFF, int & 0xFF)
+        case 8: // ARGB
+            (a, r, g, b) = ((int >> 24) & 0xFF, (int >> 16) & 0xFF, (int >> 8) & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (255, 0, 0, 0) // Default to black
+        }
+        
+        self = Color(.sRGB, red: Double(r) / 255, green: Double(g) / 255, blue: Double(b) / 255, opacity: Double(a) / 255)
     }
 }
