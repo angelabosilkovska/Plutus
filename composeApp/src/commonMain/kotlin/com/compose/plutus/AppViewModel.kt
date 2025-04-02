7253cc20710af27948c8ea6e831955b2d40dacc5
Angela <angelabosilkovska99@gmail.com>
2025-04-02 13:24:56 +0200

    logging api call result

--- composeApp/src/commonMain/kotlin/com/compose/plutus/AppViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/compose/plutus/AppViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.compose.plutus.data.UserData
 import com.compose.plutus.network.UserDataClient
+import com.compose.plutus.util.AppLogger
 import com.compose.plutus.util.NetworkError
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -21,9 +22,11 @@ class AppViewModel (
         fetchUserData()
     }
 
-    fun fetchUserData() {
+    private fun fetchUserData() {
         viewModelScope.launch {
-            _userData.value = client.getUserData()
+            val result = client.getUserData()
+            AppLogger.d("AppViewModel", "AppViewModel result: $result")
+            _userData.value = result
         }
     }
 }
